fa35e5f5d8e04e7005b3370c552d14acfa676e4a
Eduardas <edislat22@gmail.com>
2021-02-27 23:59:41 +0200

    Many update

---
+++ js/Home.js
@@ -0,0 +1,30 @@
+// IMPORTS
+/* header */
+/* hero */
+/* about me */
+/* services */
+import { services } from './components/services/services.js';
+import { servicesData } from './data/servicesData.js';
+
+/* resume */
+/* freelance */
+/* portfolio */
+/* testimonials */
+/* blogs */
+/* contact me */
+/* footer */
+
+// EXECUTION
+/* header */
+/* hero */
+/* about me */
+/* services */
+services(servicesData);
+
+/* resume */
+/* freelance */
+/* portfolio */
+/* testimonials */
+/* blogs */
+/* contact me */
+/* footer */
